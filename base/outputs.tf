6fdb5760be5341452817e60f9d76a4a5556b2f7b
Gorkem Korkmaz <gorkeminanckorkmaz@gmail.com>
2025-10-15 22:25:08 +0300

    output updated from base module and harbor environment updated

--- base/outputs.tf
+++ base/outputs.tf
@@ -1,14 +1,10 @@
-output "vm_id" {
-  description = "The ID of the created VM"
-  value       = proxmox_virtual_environment_vm.vm.vm_id  # Replace 'this' with your actual resource name
-}
-
-output "vm_name" {
-  description = "The name of the created VM"
-  value       = proxmox_virtual_environment_vm.vm.name  # Replace 'this' with your actual resource name
-}
-
-output "vm_ip" {
-  description = "The name of the created VM"
-  value       = proxmox_virtual_environment_vm.vm.initialization[0].ip_config[0].ipv4[0].address  # Replace 'this' with your actual resource name
+# Consolidated output for easy viewing
+output "vm_details" {
+  description = "Complete VM details summary"
+  value = {
+    vm_id     = proxmox_virtual_environment_vm.vm.vm_id
+    vm_name   = proxmox_virtual_environment_vm.vm.name
+    vm_ip     = proxmox_virtual_environment_vm.vm.initialization[0].ip_config[0].ipv4[0].address
+    node_name = proxmox_virtual_environment_vm.vm.node_name
+  }
 }
